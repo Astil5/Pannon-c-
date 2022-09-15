3ff89801d17f07ac233fbea3de4c3577a91797f8
Astil5 <71120830+Astil5@users.noreply.github.com>
2022-09-15 09:26:32 +0200

    Objektum számláló
    A garbage collector automatikusan töröli azokat az objektumokat amikre nincs referencia.

---
+++ Object counter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Automatikus_szemet
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Vegtelen v = new Vegtelen();
+                
+            }
+        }
+    }
+    class Vegtelen
+    {
+        public static int ObjCounter = 0;
+        public Vegtelen()
+        {
+            ObjCounter++;
+            Console.WriteLine("Az objektumok száma: {0}", ObjCounter);
+        }
+        ~Vegtelen()
+        {
+            ObjCounter--;
+            Console.WriteLine("Az objektumok száma: {0}", ObjCounter);
+        }
+    }
+}
